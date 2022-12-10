dbc775e642702d64fe47d2110154c42124233454
Ijeona <Ijeona@users.noreply.github.com>
2022-12-10 11:26:44 +0800

    deleted rem from styles width , migrated models.py

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class NewsStory(models.Model):
     class Meta:
-        ordering =['-pub_date']
+        ordering =['-pub_date'] 
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
         get_user_model(),
