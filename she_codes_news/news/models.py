52a133da914bc1b8c5d900618e8fe86e06ad5ca1
Ijeona <Ijeona@users.noreply.github.com>
2022-12-10 11:11:48 +0800

    Addded in views and changed

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 
 class NewsStory(models.Model):
+    class Meta:
+        ordering =['-pub_date']
     title = models.CharField(max_length=200)
     author = models.ForeignKey(
         get_user_model(),
