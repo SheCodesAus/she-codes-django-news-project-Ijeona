6f25f381382a0c9223c0990489ccd653468570fa
Ijeona <Ijeona@users.noreply.github.com>
2022-12-12 16:10:03 +0800

    commit chnaged

--- she_codes_news/users/views.py
+++ she_codes_news/users/views.py
@@ -3,12 +3,17 @@ from django.views.generic.edit import CreateView
 from django.views import generic
 from .models import CustomUser
 from .forms import CustomUserCreationForm
+from .forms import CustomUserChangeForm
 class CreateAccountView(CreateView):
 	form_class = CustomUserCreationForm
 	success_url = reverse_lazy('login')
 	template_name = 'users/createAccount.html'
-
 class ProfileView(generic.DetailView):
-	model=CustomUser
-	template_name: 'user/profile.html'
-	context_object_name: 'profile'
+    model = CustomUser
+    template_name = 'users/profile.html'
+    context_object_name = 'profile'
+class EditAccountView(generic.UpdateView):
+    form_class = CustomUserChangeForm
+    model = CustomUser
+    context_object_name = 'createAccount'
+    template_name = 'users/createAccount.html'
