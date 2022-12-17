ca5d6fb3d93a73303b6389efaec6250ee9383d51
Ijeona <Ijeona@users.noreply.github.com>
2022-12-17 10:16:55 +0800

    Added in the load page after editing

--- she_codes_news/news/models.py
+++ she_codes_news/news/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
 
 class NewsStory(models.Model):
@@ -13,3 +14,6 @@ class NewsStory(models.Model):
     pub_date = models.DateTimeField()
     content = models.TextField()
     image_url_field = models.URLField(max_length=200, null=True)
+
+    def get_absolute_url(self):
+     return reverse('news:story',kwargs={'pk': self.pk})
